be4c409474d1b9f39c24019817163cb723615fea
marcosfrdg <84476754+marcosfrdg@users.noreply.github.com>
2021-05-20 15:11:09 -0300

    HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ export function initConfig(appConfig: ConfiguracaoService) {
 }
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
   providers: [
